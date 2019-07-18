0afae01a9a859afe1bba3f46400e11db750621e7
Paul Smith <code@uvwxy.de>
2019-07-18 21:50:47 +0200

    play around with the fontencoding

--- src/util/U8G2Font.ts
+++ src/util/U8G2Font.ts
@@ -40,6 +40,14 @@ export interface FontGlyph {
     glyph: number;
     char: string;
     nextGlyphOffset: number;
+    charWidth: number; // unsigned
+    charHeight: number; // unsigned
+    xOffset: number; // signed
+    yOffset: number; // signed
+    characterPitch: number; // signed
+    dataBitStart: number;
+    dataBinary: string;
+    dataBinaryList: string[];
 
 }
 
@@ -83,15 +91,71 @@ export const getFontInfo = (buf: number[]) => {
     return fontInfo;
 };
 
-export const getGlyphs = (buf: number[]): FontGlyph[] => {
+const toBinary = (n: number) => {
+    let str = n.toString(2);
+    // hit: I was tired and uncreative here ...
+    switch (str.length) {
+        case 8:
+            return str;
+        case 7:
+            return "0" + str;
+        case 6:
+            return "00" + str;
+        case 5:
+            return "000" + str;
+        case 4:
+            return "0000" + str;
+        case 3:
+            return "00000" + str;
+        case 2:
+            return "000000" + str;
+        case 1:
+            return "0000000" + str;
+        default:
+            return "00000000";
+    }
+};
+
+export const getGlyphs = (buf: number[], fontInfo: FontInfo): FontGlyph[] => {
     let offset: number = 23;
 
     const getNextGlyph = (n: number, unicode: boolean = false): FontGlyph => {
         const glyph = unicode ? buf[n] << 8 + buf[n + 1] : buf[n];
+        const nextGlyphOffset = buf[n + (unicode ? 2 : 1)];
+        const metaBitStart = 8 * (unicode ? 3 : 2);
+        const dataBinaryList = buf.slice(n, n + nextGlyphOffset).map(toBinary);
+        const dataBinary = dataBinaryList.join("");
+
+        let next = metaBitStart;
+        const charWidth = parseInt(dataBinary.slice(next, next + fontInfo.bits_per_char_width), 2);
+        next += fontInfo.bits_per_char_width;
+
+        const charHeight = parseInt(dataBinary.slice(next, next + fontInfo.bits_per_char_height), 2);
+        next += fontInfo.bits_per_char_height;
+
+        const xOffset = parseInt(dataBinary.slice(next, next + fontInfo.bits_per_char_x), 2) - (1 << (fontInfo.bits_per_char_x - 1));
+        next += fontInfo.bits_per_char_x;
+
+        const yOffset = parseInt(dataBinary.slice(next + 1, next + fontInfo.bits_per_char_y), 2) - (1 << (fontInfo.bits_per_char_y - 1));
+        next += fontInfo.bits_per_char_y;
+
+        const characterPitch = parseInt(dataBinary.slice(next, next + fontInfo.bits_per_delta_x), 2) - (1 << (fontInfo.bits_per_delta_x - 1));
+        next += fontInfo.bits_per_delta_x;
+
+        const dataBitStart = next;
+
         return {
             glyph,
-            nextGlyphOffset: buf[n + (unicode ? 2 : 1)],
-            char: String.fromCharCode(glyph)
+            char: String.fromCharCode(glyph),
+            nextGlyphOffset,
+            charWidth,
+            charHeight,
+            xOffset,
+            yOffset,
+            characterPitch,
+            dataBitStart,
+            dataBinary,
+            dataBinaryList
         };
     };
 
@@ -106,4 +170,30 @@ export const getGlyphs = (buf: number[]): FontGlyph[] => {
     } while (glyph.nextGlyphOffset < buf.length && glyph.nextGlyphOffset > 0);
 
     return glyphs;
+};
+
+export const getGlyphBitmap = (glyph: FontGlyph, fontInfo: FontInfo) => {
+
+    // bitcntW 	glyph bitmap width (variable width)
+    // bitcntH 	glyph bitmap height (variable width)
+    // bitcntX 	glyph bitmap x offset (variable width)
+    // bitcntY 	glyph bitmap y offset (variable width)
+    // bitcntD 	character pitch (variable width)
+
+    // sequence:
+    const bits_per_0 = fontInfo.bits_per_0; // m0 Bits (see font header) denoting the number of zeros
+    const bits_per_1 = fontInfo.bits_per_1; // m1 Bits (see font header) denoting the number of ones
+    // n Bits == 1 (to be counted) denoting the number of repetitions of the sequence and
+    // 1 Bit == 0 as stop marker for each sequence.
+
+    // 16 01000001 00010101
+    // 20 1010 -> 10
+    // 24 1010 -> 10
+    // 28 0000 -> 0
+    // 33 10001 -> ??
+    // 38 01101 -> 13
+    // 0111 101000000100000011100110000000000111001000101010001010100001000010001010100001010011010010100000010010010010100100100000000100',
+    // 0111101000000100000011100110000000000111001000101010001010100001000010001010100001010011010010100000010010010010100100100000000100',
+
+    // '010000010001010110101010000010001011010111101000000100000011100110000000000111001000101010001010100001000010001010100001010011010010100000010010010010100100100000000100',
 };
--- src/util/U8G2FontParser.ts
+++ src/util/U8G2FontParser.ts
@@ -1,6 +1,11 @@
 import { getFontInfo, getGlyphs } from "./U8G2Font";
 import { u8g2_font_courB12_tf } from "../fonts/u8g2_font_courB12_tf";
 
-console.log(getFontInfo(u8g2_font_courB12_tf));
+const fontInfo = getFontInfo(u8g2_font_courB12_tf);
+console.log(fontInfo);
 
-console.log(getGlyphs(u8g2_font_courB12_tf));
+const glyphs = getGlyphs(u8g2_font_courB12_tf, fontInfo);
+
+const glyphA = glyphs.filter(g => g.char === "A")[0];
+
+console.log(glyphA);
